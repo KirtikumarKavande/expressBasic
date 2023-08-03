6f90fe6b32fceb4bba94ba9d18666625a24731cb
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-08-03 10:48:32 +0530

    intro to middleware

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-const http = require("http");
 const express = require("express");
 
 const app = express();
-app.use((req, res, next) => {
-  console.log("In 1st middleware");
-  next();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product'   method='POST'><input name='title' placeholder='enter product name'/> <input name='quantity' placeholder='Quantity'/><button>add product</button></form>"
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("In 2nd middleware");
-  res.send({ key1: "1" });
+app.post("/product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/add-product");
 });
 
-app.listen(3000);
+app.use("/", (req, res) => {
+  res.end("hello express tut");
+});
+
+app.listen(4000);
